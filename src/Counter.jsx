5d386fcf3473ced994a03f0795da064d78e72ba4
Om Rakibe <rakibeom30@gmail.com>
2024-12-14 08:49:37 +0530

    Learn and tried useState() hook

---
+++ src/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Counter() {
+  
+  //useState():
+  let [stateVariable, setStateVariable] = useState(10);
+  let [count, setCount] = useState(0);
+
+   let incCount = () => {
+    setCount(count + 1)
+    // count += 1;
+    console.log(count);
+   }
+
+  return (
+    <>
+      <button onClick={incCount}>Count {count}</button>
+    </>
+  );
+}
+
+export default Counter;
