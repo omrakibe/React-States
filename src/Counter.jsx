53d132e5538223f8da973e3e228c591c030eee82
Om Rakibe <rakibeom30@gmail.com>
2024-12-19 08:51:41 +0530

    Learn about updater FUnction and closure

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,9 +5,25 @@ function Counter() {
   //useState():
   let [stateVariable, setStateVariable] = useState(10);
   let [count, setCount] = useState(0);
+  console.log("Component is render!!");
+  console.log(`Count is ${count}`);
+  
 
    let incCount = () => {
-    setCount(count + 1)
+
+    setCount(count += 1); //this is an Asynchronus function
+    setCount (count += 1);
+
+    // setCount((currCount) => { //this is a Synchronus function
+    //   return currCount += 1;
+    // })
+
+    // setCount((nextCount) => {
+    //   return nextCount += 1;
+    // })
+
+    console.log(`inside inCount, Count is ${count}`);
+    
     // count += 1;
     // console.log(count);
    }
@@ -20,3 +36,4 @@ function Counter() {
 }
 
 export default Counter;
+
